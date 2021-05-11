5663336e349a1aba4d42269338964ae21d6792b9
vipenl26 <vipenl26@gmail.com>
2021-05-11 19:06:34 +0530

    added a button

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.kotlindemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn = findViewById<Button>(R.id.btn)
+        var txt = findViewById<TextView>(R.id.txt);
+
+        var flag=false;
+        btn.setOnClickListener(View.OnClickListener {
+            // Code here executes on main thread after user presses button
+            if(flag){
+                txt.text = "Aak is Pak"
+                flag = false;
+            }
+            else {
+                txt.text = "Pak is Aak"
+                flag = true;
+            }
+
+        })
+
     }
 }
+
---
+++ app/src/main/java/com/example/kotlindemo/main.kt
@@ -0,0 +1,12 @@
+package com.example.kotlindemo
+
+fun greet(name:String){
+    println("Hello $name, how are you doing?")
+}
+
+fun main(){
+    println("hello kotlin")
+//    var name:String="vipen"
+//    greet(name)
+    println("testing android app")
+}
